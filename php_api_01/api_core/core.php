ab0ceb973a4f7402defc24a452a95ef1d0a933c2
Pedro Salemi <128959821+SalemiDeveloper@users.noreply.github.com>
2026-03-19 15:08:03 -0300

    Add files via upload

---
+++ php_api_01/api_core/core.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+    [
+        'name' => 'pedro',
+        'email' => 'pedro@gmail.com'
+    ],
+    [
+        'name' => 'joao',
+        'email' => 'joao@gmail.com'
+    ],
+    [
+        'name' => 'maria',
+        'email' => 'maria@gmail.com'
+    ],
+    [
+        'name' => 'bruno',
+        'email' => 'bruno@hotmail.com'
+    ],
+    [
+        'name' => 'thiago',
+        'email' => 'thiago@gmail.com'
+    ],
+    [
+        'name' => 'gustavo',
+        'email' => 'gustavo@gmail.com'
+    ],
+    [
+        'name' => 'eduardo',
+        'email' => 'eduardo@hotmail.com'
+    ],
+    [
+        'name' => 'rarafel',
+        'email' => 'rarafel@hotmail.com'
+    ],
+    [
+        'name' => 'renan',
+        'email' => ''
+    ],
+    [
+        'name' => 'julio',
+        'email' => 'julio@gmail.com'
+    ]
+];
