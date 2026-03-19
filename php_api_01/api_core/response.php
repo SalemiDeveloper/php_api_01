3eadf3fa5f8d79fb4615fd671d8a28bffc8657bb
Pedro Salemi <128959821+SalemiDeveloper@users.noreply.github.com>
2026-03-19 15:08:34 -0300

    Enhance JSON response with API status check
    Added API status check and response details.

--- php_api_01/api_core/response.php
+++ php_api_01/api_core/response.php
@@ -3,11 +3,27 @@
 class Response {
     
     public static function json($status = 200, $message = 'success', $data = null) {
+
         header('Content-Type: application/json');
+        // Checando se a API está ativa
+        if (!API_IS_ACTIVE) {
+            return json_encode([
+            'status'  => 400,
+            'message' => 'api is not ruinning',
+            'api_version'   => API_VERSION,
+            'time_response' => time(),
+            'datetime_response' => date('Y-m-d H:i:s'),
+            'data'    => null
+            ], JSON_PRETTY_PRINT);
+        }
+        
         return json_encode([
             'status'  => $status,
             'message' => $message,
+            'api_version'   => API_VERSION,
+            'time_response' => time(),
+            'datetime_response' => date('Y-m-d H:i:s'),
             'data'    => $data
-        ]);
+        ], JSON_PRETTY_PRINT);
     }
 }
